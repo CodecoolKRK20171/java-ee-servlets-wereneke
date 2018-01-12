24002bd13c100df86ab5c4029150df7f275902b8
wereneke <weronikad752@gmail.com>
2018-01-12 15:49:37 +0100

    fix dependencies

---
+++ src/main/java/com/codecool/wereneke/MiniServlet.java
@@ -0,0 +1,10 @@
+package com.codecool.wereneke;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MiniServlet {
+}
