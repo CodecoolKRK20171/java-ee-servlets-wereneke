90bfe79d4ebd80d5929adf9d6b82ce48998b7bc0
wereneke <weronikad752@gmail.com>
2018-01-12 17:07:40 +0100

    interface

--- src/main/java/com/codecool/wereneke/MiniServlet.java
+++ src/main/java/com/codecool/wereneke/MiniServlet.java
@@ -1,6 +1,11 @@
 package com.codecool.wereneke;
 
 import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,14 +17,35 @@ public class MiniServlet extends HttpServlet {
                           HttpServletResponse response)
             throws ServletException, IOException {
 
-        response.getWriter().write("<html><body>GET response</body></html>");
+        String answer = getFileWithUtil("hello.html");
+        response.getWriter().write(answer);
     }
 
     protected void doPost( HttpServletRequest request,
                            HttpServletResponse response)
             throws ServletException, IOException {
 
-        response.getWriter().write("<html><body>POST response</body></html>");
+        String future = "";
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("answer.html");
+        JtwigModel model = JtwigModel.newModel();
 
+        model.with("future", future);
+        String answer = template.render(model);
+
+        response.getWriter().write(answer);
+    }
+
+    private String getFileWithUtil(String fileName) {
+
+        String result = "";
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        try {
+            result = IOUtils.toString(classLoader.getResourceAsStream(fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
---
+++ target/classes/db/migration/v1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS future (id INTEGER PRIMARY KEY AUTOINCREMENT, future TEXT);
+
+INSERT INTO future (future) VALUES("This is a good day for meditation or spiritual studies of some kind. Your intuition should be operating at a very high level and thus it's going to be easier for you to tune in to the thoughts, feelings, needs, and desires of others.");
+INSERT INTO future (future) VALUES("You might even experience psychic insights or visions that shed light on some questions you've been asking yourself for a while. Write down your thoughts. You'll want to remember them.");
+INSERT INTO future (future) VALUES("Financial matters are going so well for you right now that you're apt to be feeling that everything will be perfect from here on out.");
+INSERT INTO future (future) VALUES("Visitors in your home might include partners in business activities, members of a group with which you're affiliated, or creative people with great ideas. Therefore, you're likely to enjoy some intense discussions today and a lot of intriguing exchanges of information.");
+INSERT INTO future (future) VALUES("Love and romance should go well, particularly since you're feeling so enthusiastic about life. Take the time to share your dreams with your guests. They might have some good ideas for making a dream a reality. ");
+INSERT INTO future (future) VALUES("Your relationships with just about everyone - friends, lovers, children, colleagues, and even strangers - should be smooth, warm, and supportive.");
+INSERT INTO future (future) VALUES(" Feelings are easily expressed, although more on a subtle than a verbal level. New business opportunities may open up through contact with new and interesting people.");
